c945615956c90d68178653fcd9f207667bc128d0
cris <c.cheuquel01@ufromail.cl>
2025-03-29 23:53:51 -0300

    fix(Controllers): fix typos in UfController
    typos in UfController has been fixed and deleted needless imports

--- src/main/java/com/isoft/actividad1/controllers/UfController.java
+++ src/main/java/com/isoft/actividad1/controllers/UfController.java
@@ -1,11 +1,8 @@
 package com.isoft.actividad1.controllers;
 
 // Dependencies
-import com.isoft.actividad1.services.CsvDataLoaderService;
-import com.isoft.actividad1.services.PrettyTable;
-
+import com.isoft.actividad1.services.PrettyTables;
 import org.jsoup.nodes.Document;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,14 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UfController {
 
-    @Autowired
-    CsvDataLoaderService ufData;
-
     @GetMapping(value = "/valor-uf", produces = "text/html")
     @ResponseBody
     public String valor_uf() {
         final String UF_PATH = "src/main/resources/shared/UF-2024.csv";
-        Document doc = PrettyTable.createTable(UF_PATH);
+        Document doc = PrettyTables.createTable(UF_PATH);
         return doc.toString();
     }
 }
