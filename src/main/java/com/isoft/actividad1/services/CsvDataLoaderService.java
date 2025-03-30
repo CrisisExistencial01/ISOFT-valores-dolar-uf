2e7761d6d0ffabe35a27f4d49fcfc5c1f9b753c0
cris <c.cheuquel01@ufromail.cl>
2025-03-29 23:21:14 -0300

    feat(services): CsvDataLoaderService class correctly added
    CsvDataLoaderService is a class decored as Service, located in package
    "services".
    
    - parameters: filename                  is the csv file's path (String)
    - return type: List<String[]>           a 2-dim String List

--- src/main/java/com/isoft/actividad1/services/CsvDataLoaderService.java
+++ src/main/java/com/isoft/actividad1/services/CsvDataLoaderService.java
@@ -1,47 +1,45 @@
+/* DOCUMENTATION
+ *
+ * USAGE:
+ * Write the decorator: \@Autowired (without the "\")
+ * ----------------------------------------------------------
+ * \@Autowired // without the "\"
+ * private CsvDataLoaderService objectName;
+ *
+ * \@whatever //please use in this ISOFT activity \@Controller
+ * // example:
+ * public YOUR_METHOD(){
+ *   // return type: A List of String's List
+ *   List <String[]> data = objectName.readCsv("path/to/the/file");
+ * }
+ * ----------------------------------------------------------
+ *
+ */
 package com.isoft.actividad1.services;
 
-/* DOCUMENTATION
-  *
-  * USAGE:
-  * Write the decorator: \@Autowired (without the "\")
-  * ----------------------------------------------------------
-  * \@Autowired // without the "\"
-  * private CsvDataLoaderService objectName;
-  *
-  * \@whatever //please use in this ISOFT activity \@Controller
-  * public YOUR_METHOD(){
-  *   // return type: A List of String's List
-  *   List <String[]> data = objectName.readCsv("path/to/the/file");
-  * }
-  * ----------------------------------------------------------
-  *
-*/
-
-import org.springframework.stereotype.Service;
-
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class CsvDataLoaderService {
-    public List<String[]> readCsv(String filename) {
+
+    public List<String[]> readCsv(String filePath) {
         List<String[]> data = new ArrayList<>();
-        try(CSVReader reader = new CSVReader(new FileReader(filename))){
-            String[] line;
-            while((line = reader.readNext()) != null) {
-                data.add(line);
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                data.add(values);
             }
-        } catch (CsvValidationException | IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
         return data;
     }
 }
-
 
